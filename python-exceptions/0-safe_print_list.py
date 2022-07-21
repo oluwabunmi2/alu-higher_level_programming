edac70abc4a8b0121ebe9a6cd05f6ca22fc31084
Oluwabunmi1 <debanjobunmi@gmail.com>
2022-07-21 04:46:33 -0700

    Safe list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-def safe_print(my_list=[], x=0):
-    total = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
-            total +=1
-        except IndexError:
-            break
-    print("")
-    return (total)
+safe_print_list = __import__('0-safe_print_list').safe_print_list
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
+
