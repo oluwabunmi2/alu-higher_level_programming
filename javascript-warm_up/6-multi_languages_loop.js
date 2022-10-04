dbfcb7d574234c14abd778ad789318a1d6e812a4
oluwabunmi2 <o.adebanjo@alustudent.com>
2022-10-04 07:23:41 -0700

    Loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+const arrV = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let a = 0; a < arrV.length; a++) {
+  console.log(arrV[a]);
 }
